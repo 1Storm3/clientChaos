aa8ffd4667f29787ac96972fc71e78f0174611d1
1Storm3 <zemskoff2013@yandex.ru>
2024-01-08 20:28:50 +0300

    2

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,19 +2,56 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import styles from "../styles/Profile.module.css";
+import axios from "axios";
 const Profile = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(
     !!localStorage.getItem("access_token")
   );
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [filePath, setFilePath] = useState("");
+
+  const onChangeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+  const onClickHandler = () => {
+    const formData = new FormData();
+    formData.append("photo", selectedFile);
+
+    axios
+      .post("https://chaoschat.onrender.com/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+      })
+      .then((response) => {
+        setFilePath(response.data.path);
+      })
+      .catch((error) => {
+        console.error("error load file", error);
+      });
+  };
+
   return (
     <div>
       <div className={styles.header}>
         <div className={styles.user}></div>
         <Link to={"/sign"}>
-          <div className={styles.linkToSign}>Вход в чат</div>
+          <div className={styles.linkToSign}>Назад</div>
         </Link>
-        <div className={styles.loadPhoto}>Загрузка фото</div>
-        <input type="file"></input>
+        <input type="file" onChange={onChangeHandler}></input>
+        <button className={styles.button} onClick={onClickHandler}>
+          Загрузить фото
+        </button>
+        {filePath && (
+          <div>
+            <h2>Фото профиля</h2>
+            <img
+              src={`https://chaoschat.onrender.com/${filePath}`}
+              alt="Загруженное изображение"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
